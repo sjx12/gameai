0233fd77c302d397ff4479438c4e3f18562a6eac
shijiaixin <shijiaxin@yanxiu.com>
2024-05-30 20:08:47 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.VUE_APP_BASE_URL, // 署应用包时的基本 URL。 vue-router hash 模式使用
+  publicPath: './', // 署应用包时的基本 URL。 vue-router hash 模式使用
   outputDir: 'dist', //  生产环境构建文件的目录
   assetsDir: 'static', //  outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: false, //eslint 检测
-  productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: true, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   devServer: {
     hot: true,
     port: 8080,
